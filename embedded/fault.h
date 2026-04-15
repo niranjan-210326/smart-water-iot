4080138f540dee066a64a10e46b80806935083e6
niranjanmp <mpnniranjanpavithra2@gmail.com>
2026-04-15 23:16:09 +0530

    Advanced fault protection: overfill, timeout, sensor stability + safety override

--- embedded/fault.h
+++ embedded/fault.h
@@ -4,8 +4,12 @@
 #define FAULT_NONE 0
 #define FAULT_DRY_RUN 1
 #define FAULT_EMPTY_SUMP 2
+#define FAULT_OVERFILL 3
+#define FAULT_TIMEOUT 4
+#define FAULT_SENSOR_ERROR 5
 
 int detect_fault(int prev_level, int current_level, int motor_state);
+int fault_is_critical(int fault_code);
 const char *fault_to_string(int fault_code);
 
 #endif /* FAULT_H */
